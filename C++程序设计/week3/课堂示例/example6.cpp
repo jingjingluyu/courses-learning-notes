7e96611a462b57e3ef43b293227585e5f0e679c6
jingjingluyu <1550816456@qq.com>
2017-08-11 15:40:41 +0800

    第三课 成员对象和封闭类 封闭类例子

---
+++ C++程序设计/week3/课堂示例/example6.cpp
@@ -0,0 +1,64 @@
+/*第三课
+成员对象和封闭类*/
+
+class CTyre{  //轮胎类
+private:
+ int radius;  //半径
+ int width; //宽度
+public:
+ CTye(int r int w):radius(r),width(w){ } //初始化，radius=r,width=w
+ };
+ class CEngine{     //引擎类
+ };
+ class CCar{   //汽车类->"封闭类"
+ private:
+   int price;  //价格
+   CTyre tyre;
+   CEngine engine;
+ public:
+   CCar(int p,int tr,int tw);
+ };
+ CCar::CCar(int p,int tr,int w):price(p),tyre(tr,w){
+
+ };
+ int main(){
+   CCar car(20000,17,225);
+   return 0;
+ }
+ /*********************************************************************************/
+ /*封闭类例子程序*/
+class CTyre{
+public:
+  CTyre(){cout<<"CTyre contructor"<<endl;}
+  ~CTyre(){cout<<"CTyre destructor"<<endl;}
+};
+class CEngine{
+public:
+  CEngine(){cout<<"CEngine contructor"<<endl;}
+  ~CEngine(){cout<<"CEngine destructor"<<endl;}
+};
+class CCar{
+private:
+  CEngine engine;
+  CTyre tyre;
+public:
+  CCar(){cout<<"CCar contructor"<<endl;}
+  ~CCar(){cout<<"CCar destructor"<<endl;}
+};
+int main(){
+  CCar car;
+  return 0;
+}
+/*结果
+CCar中的成员对象按顺序：
+CEngine contructor
+CTyre contructor
+CCar封闭类自身的构造函数：
+CCar contructor
+int main 中car要消亡时：
+封闭类的析构函数首先被调用：
+CCar destructor
+按逆序：
+CTyre destructor
+CEngine destructor
+*/
