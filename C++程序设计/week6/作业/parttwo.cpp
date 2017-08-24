aa88a9024e9a1beb520ec55a8b5535a5c59f650e
jingjingluyu <1550816456@qq.com>
2017-08-24 14:50:59 +0800

    作业二

---
+++ C++程序设计/week6/作业/parttwo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class A {
+public:
+   virtual ~A(){cout<<"destructor A"<<endl;} //虚析构函数
+  // 在此处补充你的代码
+};
+class B:public A {
+    public:
+        ~B() { cout << "destructor B" << endl; }
+};
+int main() {
+    A * pa;
+    pa = new B;
+    delete pa;
+    return 0;
+}
