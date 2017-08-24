77c95bcb7c762b12b1a906ac2e3763cb90ee0fe0
jingjingluyu <1550816456@qq.com>
2017-08-24 13:14:24 +0800

    1

---
+++ C++程序设计/week5/作业/one.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include<string>
+using namespace std;
+
+class MyString:public string{
+MyString():string(){};
+MyString(const string & str):string(str){}; //类型转换构造函数
+MyString(const char*str):string(str){};
+MyString(MyString & mystr):string(mystr){};
+MyString operator()(int start,int length)
+{
+  return this->substr(start,length);
+}
+
+};// 在此处补充你的代码
+int CompareString( const void * e1, const void * e2) {
+    MyString * s1 = (MyString * ) e1;
+    MyString * s2 = (MyString * ) e2;
+    if( *s1 < *s2 )     return -1;
+    else if( *s1 == *s2 ) return 0;
+    else if( *s1 > *s2 ) return 1;
+}
+int main() {
+    MyString s1("abcd-"),s2,s3("efgh-"),s4(s1);
+    MyString SArray[4] = {"big","me","about","take"};//调用类型转换构造函数
+    cout << "1. " << s1 << s2 << s3<< s4<< endl;
+    s4 = s3;  //调用复制构造函数
+    s3 = s1 + s3;
+    cout << "2. " << s1 << endl;
+    cout << "3. " << s2 << endl;
+    cout << "4. " << s3 << endl;
+    cout << "5. " << s4 << endl;
+    cout << "6. " << s1[2] << endl;
+    s2 = s1;    s1 = "ijkl-";
+    s1[2] = 'A' ;
+    cout << "7. " << s2 << endl;
+    cout << "8. " << s1 << endl;
+    s1 += "mnop";
+    cout << "9. " << s1 << endl;
+    s4 = "qrst-" + s2;
+    cout << "10. " << s4 << endl;
+    s1 = s2 + s4 + " uvw " + "xyz";
+    cout << "11. " << s1 << endl;
+    qsort(SArray,4,sizeof(MyString), CompareString);
+    for( int i = 0;i < 4;++i )
+        cout << SArray[i] << endl;
+    //输出s1从下标0开始长度为4的子串
+    cout << s1(0,4) << endl;
+    //输出s1从下标为5开始长度为10的子串
+    cout << s1(5,10) << endl;
+    return 0;
+}
