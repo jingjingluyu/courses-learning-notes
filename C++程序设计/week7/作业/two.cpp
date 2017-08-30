fc5468212d4f763a24264778c5f936cb45f737c7
jingjingluyu <1550816456@qq.com>
2017-08-30 09:42:05 +0800

    流操纵算子

--- C++程序设计/week7/作业/two.cpp
+++ C++程序设计/week7/作业/two.cpp
@@ -5,6 +5,6 @@ int main(int argc,char const *argv[]){
   double x;
   cin>>x;
   cout<<fixed<<setprecision(5)<<x<<endl;
-  cout<<setprecision(7)<<scientific<<x<<endl; 
-
+  cout<<setprecision(7)<<scientific<<x<<endl;
+  return 0;
 }
