ef4a20df38bb38d7879f2d8d67628034269db3ef
jingjingluyu <1550816456@qq.com>
2017-08-30 09:40:45 +0800

    科学计数法

---
+++ C++程序设计/week7/作业/two.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(int argc,char const *argv[]){
+  double x;
+  cin>>x;
+  cout<<fixed<<setprecision(5)<<x<<endl;
+  cout<<setprecision(7)<<scientific<<x<<endl; 
+
+}
