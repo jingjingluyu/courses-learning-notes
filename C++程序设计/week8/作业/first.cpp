dfed119805e6a4b89efc2dee6c863612a659e8be
jingjingluyu <1550816456@qq.com>
2017-09-09 21:31:27 +0800

    提交作业一

---
+++ C++程序设计/week8/作业/first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iterator>
+#include <set>
+using namespace std;
+int main() {
+int a[] = {8,7,8,9,6,2,1};
+set <int> v(a,a+7);
+// 在此处补充你的代码
+ostream_iterator<int> o(cout," ");
+copy( v.begin(),v.end(),o);
+return 0;
+}
